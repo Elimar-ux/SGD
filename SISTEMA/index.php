0552dce3ac4f89ad5ff93fedb1f7923baefb95e8
Elimar Alves <elimaralves_@hotmail.com>
2022-11-04 20:19:27 -0300

    Logo na pagina home
    alterando index

--- SISTEMA/index.php
+++ SISTEMA/index.php
@@ -58,8 +58,9 @@ body, html {
 
 <!-- Header with full-height image -->
 <header class="bgimg-1 w3-display-container w3-grayscale-min" id="home">
-  <div class="w3-display-left w3-text-white" style="padding:48px">
-    <p><a href="Paginas/orcamento.php" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Fazer um orçamento</a></p>
+  <div class="w3-display-left w3-text-white" style="margin-left: 10%;">
+  <img src="images/logo.png" height="400" width="400" style="margin-top: 30%;" >
+    <p><a href="Paginas/orcamento.php" style="margin: 85px" class="w3-button w3-white w3-padding-large w3-large w3-margin-top w3-opacity w3-hover-opacity-off">Fazer um orçamento</a></p>
   </div> 
   <div class="w3-display-bottomleft w3-text-grey w3-large" style="padding:24px 48px">
     <i class="fa fa-facebook-official w3-hover-opacity"></i>
@@ -113,7 +114,7 @@ body, html {
     <a href="https://www.instagram.com/choppdrivejp/" class="fa fa-instagram w3-hover-opacity"></a>
 
   </div>
-  <p>Design da pagina por <a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green">w3.css</a></p>
+
 </footer>
 </body>
 </html>
